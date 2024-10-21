3f610b473c6e5454ad3478912a0cbe595cef6816
Naveed <naveedfarooq692@gmail.com>
2024-10-21 15:50:09 +0500

    Update

--- MardoERP/settings.py
+++ MardoERP/settings.py
@@ -123,10 +123,11 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(PRODUCT_DIR, 'static/')
 
-STATICFILES_DIRS = [
-    os.path.join(PRODUCT_DIR, "static"), '/var/www/static/',
-]
+# STATICFILES_DIRS = [
+#     os.path.join(PRODUCT_DIR, "static"), '/home/ubuntu/project/Mardo-Erp /static/',
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

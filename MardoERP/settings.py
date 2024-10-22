a7949d48663fbd7a0fd3d541c61351b65eb4e4a7
Naveed <naveedfarooq692@gmail.com>
2024-10-22 12:18:47 +0500

    update

--- MardoERP/settings.py
+++ MardoERP/settings.py
@@ -125,7 +125,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATIC_ROOT = 'static/'
 
 STATICFILES_DIRS = [
     os.path.join(PRODUCT_DIR, "static"),
